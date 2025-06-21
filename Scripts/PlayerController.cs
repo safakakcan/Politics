6d15f31b55fcbd18dcadf8713ee6399399efd9ae
safakakcan <safak.dev@gmail.com>
2025-06-21 14:16:18 +0300

    WIP: PlayerController

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,21 +3,68 @@ using System;
 
 public partial class PlayerController : Camera2D
 {
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventScreenTouch touch)
-        {
-            
-        }
-    }
+	[Export] public uint FactionId;
+	
+	private Vector2 dragStartPos;
+	private Vector2 cameraStartPos;
+	private bool dragging = false;
 
-    public override void _Process(double delta)
-    {
-        
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventScreenTouch touchEvent)
+		{
+			if (touchEvent.Pressed && !IsMouseOverUI())
+			{
+				dragging = true;
+				dragStartPos = touchEvent.Position;
+				cameraStartPos = GlobalPosition;
+			}
+			else if (!touchEvent.Pressed)
+			{
+				dragging = false;
+			}
+		}
+		
+		else if (@event is InputEventMouseButton mouseEvent)
+		{
+			if (mouseEvent.ButtonIndex == MouseButton.Left && !mouseEvent.IsEcho() && !IsMouseOverUI())
+			{
+				if (mouseEvent.Pressed)
+				{
+					dragging = true;
+					dragStartPos = mouseEvent.Position;
+					cameraStartPos = GlobalPosition;
+				}
+				else
+				{
+					dragging = false;
+				}
+			}
+		}
+		
+		else if (@event is InputEventScreenDrag screenDragEvent)
+		{
+			if (!IsMouseOverUI())
+			{
+				dragging = true;
+				Vector2 delta = dragStartPos - screenDragEvent.Position;
+				GlobalPosition = cameraStartPos + delta;
+			}
+		}
+	}
 
-    public override void _Ready()
-    {
-        
-    }
+	public override void _Process(double delta)
+	{
+		if (dragging && Input.IsMouseButtonPressed(MouseButton.Left))
+		{
+			Vector2 dragDelta = dragStartPos - GetGlobalMousePosition();
+			GlobalPosition = cameraStartPos + dragDelta;
+		}
+		GD.Print(GlobalPosition);
+	}
+
+	private bool IsMouseOverUI()
+	{
+		return GetViewport().GuiGetFocusOwner() != null;
+	}
 }
--- Scripts/Unit.cs
+++ Scripts/Unit.cs
@@ -5,17 +5,17 @@ public class Unit
 	public uint UnitId;
 	public uint ArmyId;
 	public UnitType UnitType;
+	public CombatMode CombatMode;
 	public RangeValue Health;
 	public RangeValue Morale;
 	public RangeValue Energy;
 	public RangeValue Ammo;
 	public RangeValue Supply;
 	public float Experience;
-	public float MovementSpeed;
+	public float FirePower;
 	public float Resistance;
+	public float MovementSpeed;
 	public float Stealth;
 	public float Suppression;
-	public float FirePower;
-	public float Accuracy;
 	public VisionArea VisionArea;
 }
