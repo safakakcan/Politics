1765d2463f0fa84ccdcd509ca3de346062d74f7e
safakakcan <safak.dev@gmail.com>
2025-06-21 14:47:50 +0300

    WIP: PlayerController

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,70 +1,38 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class PlayerController : Camera2D
 {
 	[Export] public uint FactionId;
 	
-	private Vector2 dragStartPos;
-	private Vector2 cameraStartPos;
+	private Vector2 dragStartPosition;
 	private bool dragging = false;
 
-	public override void _Input(InputEvent @event)
+	public override void _Process(double delta)
 	{
-		if (@event is InputEventScreenTouch touchEvent)
+		if (dragging)
 		{
-			if (touchEvent.Pressed && !IsMouseOverUI())
+			if (Input.IsMouseButtonPressed(MouseButton.Left))
 			{
-				dragging = true;
-				dragStartPos = touchEvent.Position;
-				cameraStartPos = GlobalPosition;
+				var mousePosition = GetGlobalMousePosition();
+				Vector2 dragDelta = dragStartPosition - mousePosition;
+				Translate(dragDelta);
+				GlobalPosition += dragDelta;
+				dragStartPosition = mousePosition;
 			}
-			else if (!touchEvent.Pressed)
+			else
 			{
 				dragging = false;
 			}
 		}
-		
-		else if (@event is InputEventMouseButton mouseEvent)
+		else
 		{
-			if (mouseEvent.ButtonIndex == MouseButton.Left && !mouseEvent.IsEcho() && !IsMouseOverUI())
+			if (Input.IsMouseButtonPressed(MouseButton.Left))
 			{
-				if (mouseEvent.Pressed)
-				{
-					dragging = true;
-					dragStartPos = mouseEvent.Position;
-					cameraStartPos = GlobalPosition;
-				}
-				else
-				{
-					dragging = false;
-				}
-			}
-		}
-		
-		else if (@event is InputEventScreenDrag screenDragEvent)
-		{
-			if (!IsMouseOverUI())
-			{
+				dragStartPosition = GetGlobalMousePosition();
 				dragging = true;
-				Vector2 delta = dragStartPos - screenDragEvent.Position;
-				GlobalPosition = cameraStartPos + delta;
 			}
 		}
-	}
-
-	public override void _Process(double delta)
-	{
-		if (dragging && Input.IsMouseButtonPressed(MouseButton.Left))
-		{
-			Vector2 dragDelta = dragStartPos - GetGlobalMousePosition();
-			GlobalPosition = cameraStartPos + dragDelta;
-		}
-		GD.Print(GlobalPosition);
-	}
-
-	private bool IsMouseOverUI()
-	{
-		return GetViewport().GuiGetFocusOwner() != null;
 	}
 }
